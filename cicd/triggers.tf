48fb0e718ff27a6a574965707cd96a841aea93a1
Jeff Bryner <jeff@jeffbryner.com>
2025-06-28 15:39:54 -0700

    rm scheduled trigger, use service account

--- cicd/triggers.tf
+++ cicd/triggers.tf
@@ -13,9 +13,10 @@
 # ================== Triggers for "prod" environment ==================
 
 resource "google_cloudbuild_trigger" "validate_prod" {
-  project     = google_project.cicd.project_id
-  name        = "tf-validate-prod"
-  description = "Terraform validate job triggered on push event."
+  project         = google_project.cicd.project_id
+  name            = "tf-validate-prod"
+  description     = "Terraform validate job triggered on push event."
+  service_account = "projects/${local.project_id}/serviceAccounts/${local.cloudbuild_sa_email}"
 
   # if needed, trigger on only specific file changes.
   # included_files = [
@@ -41,9 +42,10 @@ resource "google_cloudbuild_trigger" "validate_prod" {
 }
 
 resource "google_cloudbuild_trigger" "plan_prod" {
-  project     = google_project.cicd.project_id
-  name        = "tf-plan-prod"
-  description = "Terraform plan job triggered on push event."
+  project         = google_project.cicd.project_id
+  name            = "tf-plan-prod"
+  description     = "Terraform plan job triggered on push event."
+  service_account = "projects/${local.project_id}/serviceAccounts/${local.cloudbuild_sa_email}"
 
   # if needed, trigger on only specific file changes.
   # included_files = [
@@ -68,64 +70,13 @@ resource "google_cloudbuild_trigger" "plan_prod" {
   ]
 }
 
-# Create another trigger as Pull Request Cloud Build triggers cannot be used by Cloud Scheduler.
-resource "google_cloudbuild_trigger" "plan_scheduled_prod" {
-  # Always disabled on push to branch.
-  disabled    = true
-  project     = google_project.cicd.project_id
-  name        = "tf-plan-scheduled-prod"
-  description = "Terraform plan job triggered on schedule."
-
-  # if needed, trigger on only specific file changes.
-  # included_files = [
-  #   "./**",
-  # ]
-
-  trigger_template {
-    repo_name   = local.repo_name
-    branch_name = "^main$"
-  }
-
-  filename = "cicd/configs/tf-plan.yaml"
-
-  substitutions = {
-    _TERRAFORM_ROOT = "."
-    _MANAGED_DIRS   = "cicd cloudrun"
-  }
-
-  depends_on = [
-    google_project_service.services,
-    google_sourcerepo_repository.configs,
-  ]
-}
-
-resource "google_cloud_scheduler_job" "plan_scheduler_prod" {
-  project          = google_project.cicd.project_id
-  name             = "plan-scheduler-prod"
-  region           = "us-east1"
-  schedule         = "0 12 * * *"
-  time_zone        = "America/New_York" # Eastern Standard Time (EST)
-  attempt_deadline = "60s"
-  http_target {
-    http_method = "POST"
-    oauth_token {
-      scope                 = "https://www.googleapis.com/auth/cloud-platform"
-      service_account_email = google_service_account.cloudbuild_scheduler_sa.email
-    }
-    uri  = "https://cloudbuild.googleapis.com/v1/${google_cloudbuild_trigger.plan_scheduled_prod.id}:run"
-    body = base64encode("{\"branchName\":\"main\"}")
-  }
-  depends_on = [
-    google_project_service.services,
-    google_app_engine_application.cloudbuild_scheduler_app,
-  ]
-}
 
 resource "google_cloudbuild_trigger" "apply_prod" {
-  disabled    = true
-  project     = google_project.cicd.project_id
-  name        = "tf-apply-prod"
-  description = "Terraform apply job triggered on push event and/or schedule."
+  disabled        = true
+  project         = google_project.cicd.project_id
+  name            = "tf-apply-prod"
+  description     = "Terraform apply job triggered on push event and/or schedule."
+  service_account = "projects/${local.project_id}/serviceAccounts/${local.cloudbuild_sa_email}"
 
   # if needed, trigger on only specific file changes.
   # included_files = [
